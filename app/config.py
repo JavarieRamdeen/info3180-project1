2f799c831666a9d4611f8c5886d27c0759849191
Javarie Ramdeen <JavarieRamdeen@gmail.com>
2022-03-20 20:07:06 -0500

    heroku

--- app/config.py
+++ app/config.py
@@ -1,11 +1,11 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # load environment variables from .env if it exists.
 
 class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
-    DATABASE_URL='postgres://okwtglrfkgtgba:b4253e0096dd33e04cb4fff38b4b80174beb0e52e95878f176d68ebd89ea1f5d@ec2-52-202-152-4.compute-1.amazonaws.com:5432/d4lho342c7n69a'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
